38cafae286925d25ac141322b63c3cd5a77ed329
DmytroPshen <mailforajob49@gmail.com>
2021-04-05 13:24:18 +0300

    add new group routes for crud pages

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,8 +15,30 @@
 //    return view('welcome');
 //});
 Route::group(['middleware'=>'web'], function (){
+
     Route::match(['get','post'],'/',['uses'=>'IndexController@execute','as'=>'home']);
     Route::get('/page/{alias}',['uses'=>'PageController@execute','as'=>'page']);
 
     Route::auth();
 });
+
+//admin
+Route::group(['prefix'=>'admin','middleware'=>'auth', function(){
+
+    //admin
+    Route::get('/', function (){
+
+    });
+
+    //admin/pages
+    Route::group(['prefix'=>'pages', function(){
+
+        //admin/pages
+        Route::get('/',['uses'=>'PagesController@execute','as'=>'pages']);
+
+        //admin/pages/add
+        Route::match(['get','post'],'/add',['uses'=>'PagesAddController@execute','as'=>'pagesAdd']);
+        //admin/edit/2
+        Route::match(['get','post','delete'],'/edit/{page}',['uses'=>'PagesEditController@execute','as'=>'pagesEdit']);
+    }]);
+}]);
