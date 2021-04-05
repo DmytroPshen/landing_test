b30451d1982d03b3e9d33a100d04d7b1ca996b48
DmytroPshen <mailforajob49@gmail.com>
2021-04-05 12:49:43 +0300

    Add new middleware

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+Route::group(['middleware'=>'web'], function (){
+    Route::match(['get','post'],'/',['uses'=>'IndexController@execute','as'=>'home']);
 });
