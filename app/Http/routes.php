628a4ead46ac18c66cfc0e667d58e5852b7ba03e
DmytroPshen <mailforajob49@gmail.com>
2021-04-05 13:38:53 +0300

    add new routes for crud portfolios

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -41,4 +41,15 @@ Route::group(['prefix'=>'admin','middleware'=>'auth', function(){
         //admin/edit/2
         Route::match(['get','post','delete'],'/edit/{page}',['uses'=>'PagesEditController@execute','as'=>'pagesEdit']);
     }]);
+
+    Route::group(['prefix'=>'portfolios'],function(){
+
+        //admin/portfolio
+        Route::get('/',['uses'=>'PortfolioController@execute','as'=>'portfolio']);
+
+        //admin/portfolio/add
+        Route::match(['get','post'],'/add',['uses'=>'PortfolioAddController@execute','as'=>'portfolioAdd']);
+        //admin/edit/2
+        Route::match(['get','post','delete'],'/edit/{portfolio}',['uses'=>'PortfolioEditController@execute','as'=>'portfolioEdit']);
+    });
 }]);
